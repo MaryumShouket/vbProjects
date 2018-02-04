2c7d9531c27f6969b7ec2d8595670ee3695bb2df
MaryumShouket <36127259+MaryumShouket@users.noreply.github.com>
2018-02-04 11:18:47 +0000

    Add files via upload

---
+++ battleshipPart1And2/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMove = New System.Windows.Forms.Label()
+        Me.txtMove = New System.Windows.Forms.MaskedTextBox()
+        Me.btnFire = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMove
+        '
+        Me.lblMove.AutoSize = True
+        Me.lblMove.Location = New System.Drawing.Point(29, 39)
+        Me.lblMove.Name = "lblMove"
+        Me.lblMove.Size = New System.Drawing.Size(64, 13)
+        Me.lblMove.TabIndex = 0
+        Me.lblMove.Text = "Enter move:"
+        '
+        'txtMove
+        '
+        Me.txtMove.Location = New System.Drawing.Point(71, 66)
+        Me.txtMove.Name = "txtMove"
+        Me.txtMove.Size = New System.Drawing.Size(100, 20)
+        Me.txtMove.TabIndex = 1
+        '
+        'btnFire
+        '
+        Me.btnFire.Location = New System.Drawing.Point(84, 117)
+        Me.btnFire.Name = "btnFire"
+        Me.btnFire.Size = New System.Drawing.Size(75, 23)
+        Me.btnFire.TabIndex = 2
+        Me.btnFire.Text = "Fire"
+        Me.btnFire.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnFire)
+        Me.Controls.Add(Me.txtMove)
+        Me.Controls.Add(Me.lblMove)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMove As Label
+    Friend WithEvents txtMove As MaskedTextBox
+    Friend WithEvents btnFire As Button
+End Class
---
+++ battleshipPart1And2/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+    Dim firePosition As String
+    Dim generator As Random
+    Dim number As Integer
+    Dim NumberOfShips As Integer = 5
+    Dim isGuessTrue As Boolean
+    Dim intFirePosition As Integer
+    Dim firePositionError As Boolean
+    Dim misses As Integer = 0
+    Dim previousFirePositionNumber As Integer = 0
+    Dim previousLocationSame As Boolean
+    Dim randomNumber As Integer
+    Dim previousFirePosition(49) As Integer
+    Dim shipLocations(49) As Integer
+    Dim ships(4) As Integer
+
+    Private Sub btnFire_Click(sender As Object, e As EventArgs) Handles btnFire.Click
+        game()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        generator = New System.Random
+        For i As Integer = 0 To 4 Step +1
+            ships(i) = generator.Next(0, 49)
+
+            ''For testing to make the two successive values same
+            'If (i = 2) Then
+            '    ships(i) = ships(i - 1)
+            'End If
+
+            If i > 0 Then
+                For j As Integer = 0 To i - 1 Step +1
+                    If ships(i).Equals(ships(j)) Then
+                        ships.SetValue(generator.Next(0, 49), i)
+                    End If
+                Next
+            End If
+        Next
+    End Sub
+    Private Sub game()
+        firePositionError = False
+        firePosition = txtMove.Text
+
+        If (Not firePosition Is Nothing) And (Integer.TryParse(firePosition, intFirePosition)) And (intFirePosition < 49) And (intFirePosition > -1) Then
+            isGuessTrue = False
+            previousLocationSame = False
+
+            If previousFirePositionNumber > 0 Then
+                For i As Integer = 0 To previousFirePositionNumber - 1 Step +1
+                    If (previousFirePosition(i).Equals(intFirePosition)) Then
+                        MsgBox("You cannot enter the same value again. Please try again")
+                        previousLocationSame = True
+                    End If
+                Next
+            End If
+            previousFirePosition(previousFirePositionNumber) = intFirePosition
+            previousFirePositionNumber += 1
+            If previousLocationSame = False Then
+                For i As Integer = 0 To 4 Step +1
+                    If intFirePosition.Equals(ships(i)) Then
+                        isGuessTrue = True
+                        ships(i) = -1
+                        NumberOfShips -= 1
+                        If NumberOfShips = 0 Then
+                            MsgBox("Congratulation! You have won.")
+                        Else
+                            MsgBox("Well done! You just hit a ship. The number of remaining ships is " & NumberOfShips & ". The number of misses is " & misses)
+                        End If
+                    End If
+                Next
+
+                If isGuessTrue = False Then
+                    misses += 1
+                    MsgBox("Oops! you missed the ship. The number of remaining ships is " & NumberOfShips & ". The number of misses is " & misses)
+                End If
+            Else
+                firePositionError = True
+            End If
+        Else
+            firePositionError = True
+        End If
+        If previousLocationSame = False And firePositionError = True Then
+            MsgBox("please enter a valid integer value between 0 and 49")
+        End If
+
+
+    End Sub
+
+    Private Sub txtMove_KeyDown(sender As Object, e As KeyEventArgs) Handles txtMove.KeyDown
+        If e.KeyCode = Keys.KeyCode.Enter Then
+            game()
+        End If
+    End Sub
+End Class
